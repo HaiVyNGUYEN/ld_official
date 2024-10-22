5d470af2bed9838c172e419182463535c96f88d8
vy <haivynguyen12345@gmail.com>
2024-10-22 11:25:31 +0200

    adding training tools

---
+++ utils/training_tools.py
@@ -0,0 +1,108 @@
+### Defining training and evaluation routine
+
+def train(dataloader, model, loss_fn, optimizer, device='cuda'):
+    
+    # Total size of dataset for reference
+    size = 0
+    
+    # places your model into training mode
+    model.train()
+    
+    # loss batch
+    batch_loss = {}
+    batch_accuracy = {}
+    
+    correct = 0
+    _correct = 0
+    
+    
+    
+    # Gives X , y for each batch
+    for batch, (X, y) in enumerate(dataloader):
+        
+        # Converting device to cuda
+        X, y = X.to(device), y.to(device)
+        model.to(device)
+        
+        # Compute prediction error / loss
+        # 1. Compute y_pred 
+        # 2. Compute loss between y and y_pred using selectd loss function
+        
+        y_pred = model(X)
+        loss = loss_fn(y_pred, y)
+        # Backpropagation on optimizing for loss
+        # 1. Sets gradients as 0 
+        # 2. Compute the gradients using back_prop
+        # 3. update the parameters using the gradients from step 2
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        _correct = (y_pred.argmax(1) == y).float().sum().item()
+        _batch_size = len(X)
+        
+        correct += _correct
+        
+        # Updating loss_batch and batch_accuracy
+        batch_loss[batch] = loss.item()
+        batch_accuracy[batch] = _correct/_batch_size
+        
+        size += _batch_size
+        
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}]")
+    
+    correct/=size
+    print(f"Train Accuracy: {(100*correct):>0.1f}%")
+    
+    return batch_loss , batch_accuracy
+
+def validation(dataloader, model, loss_fn, device='cuda'):
+    
+    # Total size of dataset for reference
+    size = 0
+    num_batches = len(dataloader)
+    
+    # Setting the model under evaluation mode.
+    model.eval()
+
+    test_loss, correct = 0, 0
+    
+    _correct = 0
+    _batch_size = 0
+    
+    batch_loss = {}
+    batch_accuracy = {}
+    
+    with torch.no_grad():
+        
+        # Gives X , y for each batch
+        for batch , (X, y) in enumerate(dataloader):
+            
+            X, y = X.to(device), y.to(device)
+            model.to(device)
+            pred = model(X)
+            batch_loss[batch] = loss_fn(pred, y).item()
+            test_loss += batch_loss[batch]
+            _batch_size = len(X)
+            
+            _correct = (pred.argmax(1) == y).float().sum().item()
+            correct += _correct
+            
+            size+=_batch_size
+            batch_accuracy[batch] = _correct/_batch_size
+            
+            
+            
+    
+    ## Calculating loss based on loss function defined
+    test_loss /= num_batches
+    
+    ## Calculating Accuracy based on how many y match with y_pred
+    correct /= size
+    
+    print(f"Valid Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    
+    return batch_loss , batch_accuracy, correct
