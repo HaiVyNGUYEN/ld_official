7fc5395c1f68943ca5a1b0087f26f4c5abc128c6
vy <haivynguyen12345@gmail.com>
2024-10-22 14:58:27 +0200

    ...

--- MNIST_FashionMNIST/OOD_LD.py
+++ MNIST_FashionMNIST/OOD_LD.py
@@ -3,6 +3,7 @@ import os
 current = os.path.dirname(os.path.realpath('./'))
 sys.path.append(current)
 from utils.ld_tools import *
+from model_archi.fashion_mnist_archi import MyOwnNeuralNetwork
 
 import random
 import torchvision
@@ -15,46 +16,6 @@ from sklearn import metrics
 from sklearn.preprocessing import MinMaxScaler, normalize
 from datetime import datetime
 from sklearn.cluster import KMeans
-
-
-
-class MyOwnNeuralNetwork(nn.Module):
-    def __init__(self):
-        super(MyOwnNeuralNetwork, self).__init__()
-        self.conv1 = nn.Sequential(
-              nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, padding=1),
-              nn.BatchNorm2d(64),
-              #nn.ReLU(inplace=True)
-          )
-
-        self.conv2 = nn.Sequential(
-              nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
-              nn.BatchNorm2d(128),
-              #nn.ReLU(inplace=True),
-          )
-        self.conv3 = nn.Sequential(
-              nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
-              nn.BatchNorm2d(128),
-              #nn.ReLU(inplace=True),
-          )
-
-        self.fc1 = nn.Linear(1152, 25)
-        #self.fc2 = nn.Linear(160, 25)
-        self.fc3 = nn.Linear(25, 10)  ## Softmax layer ignored since the loss function defined is nn.CrossEntropy()
-
-    def forward_before_softmax(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2(x), 2))
-        x = F.relu(F.max_pool2d(self.conv3(x), 2))
-        x = x.view(-1, 1152)
-        x = self.fc1(x)
-        return x
-    
-    
-    def forward(self, x):
-        x = self.forward_before_softmax(x)
-        x = self.fc3(x)
-        return x
 
 
 data_dir = 'dataset'
